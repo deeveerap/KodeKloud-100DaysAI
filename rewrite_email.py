ca1d46523ef78dd9bd466b69882ce6397b75116d
deeveerap <39435095+deeveerap@users.noreply.github.com>
2026-06-18 15:05:50 +0530

    Update rewrite_email.py

--- rewrite_email.py
+++ rewrite_email.py
@@ -6,23 +6,20 @@ client = OpenAI(
 )
 
 def rewrite_email(text: str) -> str:
-    system_prompt="""
-       You are expert in writing polite and professional emails
-       """
     user_prompt=f"""
        Convert this text into a polite and professional email {text}
        """
     
     response = client.chat.completions.create (
         model="openai/gpt-4.1-mini",
-        temperature= 0.1,
-        max_tokens= 60,
+        temperature=0.1,
+        max_tokens=60,
         messages = [
-            {"role":"system", "content":system_prompt},
             {"role":"user", "content":user_prompt}
         ]
     )
+    print (response.choices[0].message.content)
     return response.choices[0].message.content
 if __name__ == "__main__":
     input_msg="hey send me that report asap"
-    print (rewrite_email(input_msg))
+    rewrite_email(input_msg)
